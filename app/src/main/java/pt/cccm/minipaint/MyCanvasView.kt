28136ea48aa015ec091c0e92844d968e7793d6cc
nb26548 <nb26548@novabase.pt>
2021-02-20 16:33:59 +0000

    Added functions to draw on canvas when user touches the screen.

--- app/src/main/java/pt/cccm/minipaint/MyCanvasView.kt
+++ app/src/main/java/pt/cccm/minipaint/MyCanvasView.kt
@@ -4,25 +4,73 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 
 private  const val STROKE_WIDTH = 12f //ha to be float
 
 class MyCanvasView (context: Context): View(context) {
- private lateinit var extraCanvas: Canvas
- private lateinit var extraBitmap: Bitmap
- private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
- private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
- private  val paint = Paint().apply {
-    color = drawColor
-    isAntiAlias = true
-    isDither = true
-    style = Paint.Style.STROKE
-    strokeJoin = Paint.Join.ROUND
-    strokeCap = Paint.Cap.ROUND
-    strokeWidth = STROKE_WIDTH
-}
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private  val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    private var path = Path()
+
+    private var motionTouchEventX: Float = 0f
+    private var motionTouchEventY: Float = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private  val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchUp() {
+path.reset()
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance)
+        {
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) /2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
 
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
